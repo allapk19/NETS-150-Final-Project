da7a6bf1182cdd332a57bee5c6272c90561223e9
Praneeth Alla <praneethalla@Praneeths-MBP.fios-router.home>
2020-04-26 18:25:57 -0400

    inital commit

---
+++ src/DataCollection.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+public class DataCollection {
+    
+    private final String baseURL = "https://www.cia.gov/library/publications/the-world-factbook/";
+    private Document currentDoc;
+    
+    public DataCollection()  {
+        try {
+            this.currentDoc = Jsoup.connect(this.baseURL).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public House[] getTrainingData(String location) {
+        throw new UnsupportedOperationException("TODO: implement");
+    }
+    
+    public void main(String args[]) {
+        
+    }
+    
+    
+}
