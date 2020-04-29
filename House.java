c85751c9ae5fe4d66b50e15c5ac101a739f1a9bc
Sachin Katyal <skatyal@seas.upenn.edu>
2020-04-28 20:11:44 -0500

    Most Similar Implementation

---
+++ House.java
@@ -0,0 +1,53 @@
+
+public class House {
+
+    private int beds;
+    private int baths;
+    private int squareFootage;
+    private double price;
+    
+    public House(int beds, int baths, int squareFootage, double price) {
+        this.setBeds(beds);
+        this.setBaths(baths);
+        this.setSquareFootage(squareFootage);
+        this.setPrice(price);
+    }
+
+    public int getBeds() {
+        return beds;
+    }
+
+    public void setBeds(int beds) {
+        this.beds = beds;
+    }
+
+    public int getBaths() {
+        return baths;
+    }
+
+    public void setBaths(int baths) {
+        this.baths = baths;
+    }
+
+    public int getSquareFootage() {
+        return squareFootage;
+    }
+
+    public void setSquareFootage(int squareFootage) {
+        this.squareFootage = squareFootage;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+    @Override
+    public String toString() {
+    	return "Beds: " + this.beds + ", Baths: " + this.baths + ", Sqr Footage: " + this.squareFootage + ", Price:  " + this.price;
+    }
+    
+}
